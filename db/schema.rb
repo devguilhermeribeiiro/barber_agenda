a8a3a3aa1132065be58be6c5b0e8b987b10c3621
devguilhermeribeiro <devguilhermeribeiro@gmail.com>
2024-11-03 21:58:32 -0300

    Adding more fields to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_143956) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_183510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_143956) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "barber_code"
+    t.string "name"
+    t.string "address"
     t.index ["email"], name: "index_barbers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_barbers_on_reset_password_token", unique: true
   end
