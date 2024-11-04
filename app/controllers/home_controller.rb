907b9214d04b9edfb9934d99f24d5991911c62b6
devguilhermeribeiro <devguilhermeribeiro@gmail.com>
2024-11-03 21:52:13 -0300

    Creating home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,36 @@
+class HomeController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_barber, only: %i[ show new ]
+
+  def index
+    @barbers = Barber.all
+  end
+
+  def show
+  end
+
+  def new
+    @scheduling = Scheduling.new
+  end
+
+  def create
+    @scheduling = Scheduling.new(scheduling_params)
+    @scheduling.customer_id = current_customer.id
+
+    if @scheduling.save
+      redirect_to home_path(current_customer.id), notice: "Scheduling successfully created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_barber
+    @barber = Barber.find(params[:id])
+  end
+
+  def scheduling_params
+    params.require(:scheduling).permit(%i[ date time barber_id ])
+  end
+end
